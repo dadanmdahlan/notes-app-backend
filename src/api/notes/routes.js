934269cf8254e345e38d1df6c130412797f6ce3a
dadan-pro <dadan.muhammad13@gmail.com>
2021-07-18 11:10:34 +0700

    restructure with hapi plugin

---
+++ src/api/notes/routes.js
@@ -0,0 +1,33 @@
+/* eslint-disable linebreak-style */
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable linebreak-style */
+const routes = (handler) => [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: handler.postNoteHandler, // postNoteHandler hanya menerima dan menyimpan "satu" note.
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: handler.getNotesHandler, // getNotesHandler mengembalikan "banyak" note.
+  },
+  {
+    method: 'GET',
+    path: '/notes{id}',
+    handler: handler.getNoteByIdHandler, // getNoteByIdHandler mengembalikan "satu" note.
+  },
+  {
+    method: 'PUT',
+    path: '/notes{id}',
+    handler: handler.putNoteByIdHandler, 
+    // putNoteByIdHandler hanya menerima dan mengubah "satu" note.
+  },
+  {
+    method: 'DELETE',
+    path: '/notes{id}',
+    handler: handler.deleteNoteByIdHandler, // deleteNoteByIdhandler untuk menghapus satu note.
+  },
+];
+
+module.exports = routes;
